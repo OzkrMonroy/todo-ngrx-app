af852f5d987bce7ad97e6879e5a3d928b011ed85
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-18 08:59:19 -0600

    Display how many items are pending is working.

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  filtersTypes,
+  setFilterAction,
+} from '../../store/filter/filter.actions';
+import { AppState } from '../../app.reducer';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrls: ['./todo-footer.component.css']
+  styleUrls: ['./todo-footer.component.css'],
 })
 export class TodoFooterComponent implements OnInit {
+  currentFilter!: filtersTypes;
+  filters: filtersTypes[] = ['all', 'active', 'completed'];
+  pendingItems!: number;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.subscribe({
+      next: ({ filters, todos }) => {
+        this.currentFilter = filters;
+        this.pendingItems = todos.filter((todo) => !todo.completed).length;
+      },
+    });
   }
 
+  setFilter(filter: filtersTypes): void {
+    this.store.dispatch(setFilterAction({ filter }));
+  }
+
+  getIsOptionSelected(option: filtersTypes) {
+    return this.currentFilter === option;
+  }
 }
