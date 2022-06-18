b1d5ec80b777969dd095657383be4bc29c80609e
Oscar Monroy <oscarmonroy657@gmail.com>
2022-06-17 19:37:25 -0600

    Display the todo list is ready. When the user click twices the editing functionality is activated, and when the user moves out of the field the editing is ends.

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo!: Todo;
+  @ViewChild('editInputEl') editInputEl!: ElementRef;
 
-  constructor() { }
+  completedCheck!: FormControl;
+  editInput!: FormControl;
+  editing: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.completedCheck = new FormControl(this.todo.completed);
+    this.editInput = new FormControl(this.todo.text, [
+      Validators.required,
+      Validators.minLength(4),
+    ]);
   }
 
+  setEditing(): void {
+    this.editing = true;
+    setTimeout(() => {
+      this.editInputEl.nativeElement.select();
+    }, 1);
+  }
+  unsetEditing(): void {
+    this.editing = false;
+  }
 }
